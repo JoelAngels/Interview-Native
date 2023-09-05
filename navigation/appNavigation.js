aea60d5dcace2d7ccb92c77ab3e77b71b3ff00f0
Joel Angel <joelbaraka4325@gmail.com>
2023-09-05 18:10:22 +0300

    nav ui

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -1,5 +1,4 @@
-import { Text, StyleSheet, View } from "react-native";
-import React, { Component } from "react";
+import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
@@ -10,10 +9,12 @@ export default function AppNavigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={HomeScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,36 @@
-import { View, Text, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  Bars3CenterLeftIcon,
+  MagnifyingGlassIcon,
+  // MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+import { styles } from "../theme";
+
+const ios = Platform.OS === "ios";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className="flex-1 bg-neutral-800">
+      <SafeAreaView className="mt-3">
+        <StatusBar style="light" />
+        <View className="flex-row justify-between items-center mx-4">
+          <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+          <Text className="text-white text-3xl font-bold">
+            <Text style={styles.text}>M</Text>ovies
+          </Text>
+          <TouchableOpacity>
+            <MagnifyingGlassIcon size="30" strokeWidth={2} color="white" />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
