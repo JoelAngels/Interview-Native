3fad88ce54f60e154819bc68877b7e8bb951154c
Joel Angel <joelbaraka4325@gmail.com>
2023-09-05 13:13:08 +0300

    initialized the first component render

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -1,14 +1,19 @@
 import { Text, StyleSheet, View } from "react-native";
 import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
 
-export default class AppNavigation extends Component {
-  render() {
-    return (
-      <View>
-        <Text>appNavigatAon</Text>
-      </View>
-    );
-  }
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({});
