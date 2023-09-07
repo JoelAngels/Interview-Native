ae7e27d56c26a59b271e3c7f472fe4690adcde16
Joel Angel <joelbaraka4325@gmail.com>
2023-09-07 15:55:24 +0300

    worked on the trending component

--- components/trendingMovies.js
+++ components/trendingMovies.js
@@ -7,29 +7,36 @@ import {
 } from "react-native";
 import React from "react";
 import Carousel from "react-native-snap-carousel-v4";
+import { useNavigation } from "@react-navigation/native";
 
 var { width, height } = Dimensions.get("window");
 
 export default function TrendingMovies({ data }) {
+  const navigation = useNavigation();
+
+  const handleClick = () => {
+    navigation.navigate("Movie", item);
+  };
+
   return (
     <View className="mb-18">
-      <Text className="text-white text-xl mx-4 mb-5">Joel and purity</Text>
+      <Text className="text-white text-xl mx-4 mb-5">Trending</Text>
       <Carousel
         data={data}
         renderItem={({ item }) => <MovieCard item={item} />}
         firstItem={1}
         inactiveSlideOpacity={0.6}
-        sliderWidth={600}
-        itemWidth={400}
+        sliderWidth={width}
+        itemWidth={width * 0.62}
         slideStyle={{ display: "flex", alignItems: "center" }}
       />
     </View>
   );
 }
 
-const MovieCard = ({ item }) => {
+const MovieCard = ({ item, handleClick }) => {
   return (
-    <TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onpress={handleClick}>
       <Image
         source={require("../assets/images/moviePoster1.png")}
         style={{
