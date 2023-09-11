0d230cc466c90b4ec603c79b19f9d2679a6334cd
Joel Angel <joelbaraka4325@gmail.com>
2023-09-11 14:17:29 +0300

    fixed the onclick bug

--- components/trendingMovies.js
+++ components/trendingMovies.js
@@ -14,17 +14,21 @@ var { width, height } = Dimensions.get("window");
 export default function TrendingMovies({ data }) {
   const navigation = useNavigation();
 
-  const handleClick = () => {
+  const handleClick = (item) => {
     navigation.navigate("Movie", item);
   };
 
   return (
-    <View className="mb-18">
+    <View className="mb-8">
       <Text className="text-white text-xl mx-4 mb-5">Trending</Text>
       <Carousel
         data={data}
-        renderItem={({ item }) => <MovieCard item={item} />}
+        renderItem={({ item }) => (
+          <MovieCard item={item} handleClick={handleClick} />
+        )}
         firstItem={1}
+        loop={true}
+        inactiveSlideScale={0.86}
         inactiveSlideOpacity={0.6}
         sliderWidth={width}
         itemWidth={width * 0.62}
@@ -36,7 +40,7 @@ export default function TrendingMovies({ data }) {
 
 const MovieCard = ({ item, handleClick }) => {
   return (
-    <TouchableWithoutFeedback onpress={handleClick}>
+    <TouchableWithoutFeedback onPress={() => handleClick(item)}>
       <Image
         source={require("../assets/images/moviePoster1.png")}
         style={{
