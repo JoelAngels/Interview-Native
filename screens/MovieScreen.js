785a604963850adc442a1ad327924c3856d8b41b
Joel Angel <joelbaraka4325@gmail.com>
2023-09-25 21:26:28 +0300

    movie screen creaton

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -1,10 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ArrowLeftIcon, ChevronLeftIcon } from "react-native-heroicons/outline";
 
 export default function MovieScreen() {
   return (
-    <View>
-      <Text>MovieScreen</Text>
-    </View>
+    <ScrollView
+      contentContainerStyle={{ paddingBottom: 90 }}
+      className="flex-1 bg-neutral-900 "
+    >
+      {/* back button and movie poster */}
+      <SafeAreaView
+        className={
+          "absolute z-20 w-full flex-row justify-between items-center px-4 "
+        }
+      >
+        <TouchableOpacity
+          className="rounded-xl p-1"
+          onPress={() => navigation.goBack()}
+        >
+          <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+        </TouchableOpacity>
+      </SafeAreaView>
+    </ScrollView>
   );
 }
