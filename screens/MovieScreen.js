420166eabd4bac8fc31deeff574f87d6636f2711
Joel Angel <joelbaraka4325@gmail.com>
2023-09-26 11:31:12 +0300

    working on the movie screen icons

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -2,6 +2,8 @@ import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ArrowLeftIcon, ChevronLeftIcon } from "react-native-heroicons/outline";
+import { HeartIcon } from "react-native-heroicons/solid";
+import { styles } from "../theme";
 
 export default function MovieScreen() {
   return (
@@ -12,14 +14,27 @@ export default function MovieScreen() {
       {/* back button and movie poster */}
       <SafeAreaView
         className={
-          "absolute z-20 w-full flex-row justify-between items-center px-4 "
+          "absolute z-20 flex-row w-full justify-between items-center px-4"
         }
       >
         <TouchableOpacity
-          className="rounded-xl p-1"
-          onPress={() => navigation.goBack()}
+          style={styles.background}
+          className="rounded-xl p-1 mt-5"
         >
-          <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+          <ChevronLeftIcon
+            size="28"
+            strokeWidth={2.5}
+            color="white"
+            className="mb-9"
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity className="mt-6">
+          <HeartIcon
+            size="35"
+            color="white"
+            // color={isFavourite ? theme.background : "white"}
+          />
         </TouchableOpacity>
       </SafeAreaView>
     </ScrollView>
