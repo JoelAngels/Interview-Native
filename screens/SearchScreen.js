7b2542aab913705fa1013273ac8f299fa6090175
Joel Angel <joelbaraka4325@gmail.com>
2023-10-05 11:24:55 +0300

    worked on the screen layout ui

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,10 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { XMarkIcon } from "react-native-heroicons/outline";
+import { useNavigation } from "@react-navigation/native";
 
 export default function SearchScreen() {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>SearchScreen</Text>
-    </View>
+    <SafeAreaView className=" bg-neutral-800 flex-1 pt-5">
+      <View className="mx-4 mb-3 flex-row justify-between items-center border border-neutral-500 rounded-full">
+        <TextInput
+          // onChangeText={handleTextDebounce}
+          placeholder="Search Movie"
+          placeholderTextColor={"lightgray"}
+          className="pb-1 pl-6 flex-1 text-base font-semibold text-white tracking-wider"
+        />
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Home")}
+          className="rounded-full p-3 m-1 bg-neutral-500"
+        >
+          <XMarkIcon size="25" color="white" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
