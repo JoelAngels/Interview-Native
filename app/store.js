3125186350be0ec0e0b9a3c1e2e66b3c639dc902
JoelAngel <joelbaraka4325@gmail.com>
2023-10-06 15:08:50 +0300

    wrapping our app with redux store

--- app/store.js
+++ app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-// Or from '@reduxjs/toolkit/query/react'
-import { setupListeners } from "@reduxjs/toolkit/query";
 import { movieApi } from "../services/movieApi";
+// Or from '@reduxjs/toolkit/query/react'
+// import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
@@ -16,4 +16,4 @@ export const store = configureStore({
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
 // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
-setupListeners(store.dispatch);
+// setupListeners(store.dispatch);
